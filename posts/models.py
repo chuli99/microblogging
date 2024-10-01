89a4b2d8a8e1ba01885615292fb0d8120f305cdb
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2024-10-01 12:38:41 -0300

    #6 post model created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.content}->({self.user})"
